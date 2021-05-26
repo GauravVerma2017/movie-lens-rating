2cdcff3bd02a35ce2ccf89c493a35eb354a75a7d
vermgau <gaurav_verma@discovery.com>
2021-05-26 23:10:01 +0100

    updating comment for better readability

--- src/main/scala/com/newday/analytics/Utilities.scala
+++ src/main/scala/com/newday/analytics/Utilities.scala
@@ -8,26 +8,38 @@ import scala.io.Source
 trait Utilities {
 
   /**
-   * file schema generator
+   * getSchema reads json file from resources and returns a struct type schema
+   * @param fileName string argument
+   * @return struct type schema
    */
   def getSchema(fileName: String): StructType = {
-    val json_schema_string = Source.fromInputStream(getClass.getResourceAsStream(s"/schema/${fileName}.json")).mkString
+    val json_schema_string = Source.fromInputStream(getClass.getResourceAsStream(s"/schema/$fileName.json")).mkString
     DataType.fromJson(json_schema_string) match {
       case s: StructType => s
     }
   }
 
   /**
-   * csv reader
+   * csv_Reader reads csv files and retuns spark data frame
+   * @param spark SparkSession object
+   * @param inputPath file path string
+   * @param fileName fileName string
+   * @return spark dataframe
    */
   def csvReader(spark: SparkSession, inputPath: String, fileName: String): DataFrame = {
-    spark.read.option("delimiter", "::").schema(getSchema(fileName)).csv(s"${inputPath}/${fileName}.dat")
+    spark.read.option("delimiter", "::").schema(getSchema(fileName)).csv(s"$inputPath/$fileName.dat")
   }
 
-  def writeDF(outputDF: DataFrame, outputPath: String, resultSetName: String) = {
+  /**
+   * writeDF functins writes dataframe to output path with overwrite mode
+   * @param outputDF result dataframe
+   * @param outputPath output path string
+   * @param resultSetName tableName or resultset Name string
+   */
+  def writeDF(outputDF: DataFrame, outputPath: String, resultSetName: String):Unit = {
     // to reduce number of output part files
     val numPartitions = outputDF.sparkSession.sparkContext.defaultParallelism
-    outputDF.repartition(numPartitions).write.mode(SaveMode.Overwrite).parquet(s"${outputPath}/${resultSetName}/")
+    outputDF.repartition(numPartitions).write.mode(SaveMode.Overwrite).parquet(s"$outputPath/$resultSetName/")
   }
 
 }
